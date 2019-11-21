a0cc5d4503c25eaf82bc8634c6c1872c89e75099
veranore_sd2081@CEACCS.NET <veranore_sd2081@CEACCS.NET>
2019-11-21 09:45:20 +0800

    padayun

---
+++ src/users.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Create, Edit, List, Datagrid, TextField, EmailField, DisabledInput, SimpleForm, TextInput } from 'react-admin';
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <EmailField source="email" />
+            <TextField source="address.street" label="Address" />
+            <TextField source="phone" />
+            <TextField source="clinic" label="Clinic Name" />
+        </Datagrid>
+    </List>
+);
+
+export const UserEdit = props => (
+    <Edit {...props}>
+      <SimpleForm>
+        <DisabledInput source="id" />
+        <TextInput source="name" />
+        <TextInput source="username" />
+        <TextInput source="email" />
+        <TextInput source="address.street" label="Address" />
+        <TextInput source="phone" />
+        
+        <TextInput source="clinic" label="Clinic Name" />
+      </SimpleForm>
+    </Edit>
+  );
+
+  export const UserCreate = props => (
+    <Create {...props}>
+      <SimpleForm>
+        <DisabledInput source="id" />
+        <TextInput source="name" />
+        <TextInput source="username" />
+        <TextInput source="email" />
+        <TextInput source="address.street" label="Address" />
+        <TextInput source="phone" />
+        <TextInput source="clinic" label="Clinic Name" />
+      </SimpleForm>
+    </Create>
+  );
